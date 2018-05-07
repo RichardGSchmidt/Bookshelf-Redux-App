6f6320b63b0ba20c866b354f199f8534ca19ddeb
Richard Schmidt <richard.g.schmidt.jr@gmail.com>
2018-05-07 14:13:57 -0500

    basic interface
    Used to study react redux methods.

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+  return[
+    {title: 'Good and Evil', pages: 400},
+    {title: 'The Road', pages: 295},
+    {title: 'Maus', pages: 200},
+    {title: 'It', pages: 3000}
+  ]
+}
